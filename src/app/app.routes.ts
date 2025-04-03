24a71b02dbdf6ef578dbfd8f8bc78889ac2bcc95
lbernier <l.bernier@outlook.com>
2025-04-03 09:35:57 -0400

    feat: add Home, Header, Footer, and Todo components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Home route
+  { path: 'todo', component: TodoComponent }, // Todo route
+];
