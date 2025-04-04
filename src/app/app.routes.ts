ad46cbe0189b0e820882e949023c9d4de163a394
lbernier <l.bernier@outlook.com>
2025-04-04 18:09:58 -0400

    chore: reorganize project file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './features/home/home.component';
+import { TodoComponent } from './features/todo/todo.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Home route
